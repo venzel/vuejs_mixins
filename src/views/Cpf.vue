89177a66da91953b318221c9e48d637a6fafa594
venzel <eneas.eng@yahoo.com>
2022-03-04 23:12:25 -0300

    feat: setup

---
+++ src/views/Cpf.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>{{ cpf | cpf }}</div>
+</template>
+
+<script>
+export default {
+    filters: {
+        cpf(value) {
+            const arr = value.split('');
+
+            arr.splice(3, 0, '.');
+            arr.splice(7, 0, '.');
+            arr.splice(11, 0, '-');
+
+            return arr.join('');
+        },
+    },
+    data() {
+        return {
+            cpf: '67589190178',
+        };
+    },
+};
+</script>
+
+<style></style>
